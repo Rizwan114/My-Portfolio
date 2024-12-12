3d84d5b9b0936fc6d75397560535f2a25ae0f8f2
Rizwan114 <rizwanbaloch0099@gmail.com>
2024-12-12 23:55:19 +0500

    add someting 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Footer from "./components/Footer/Footer";
-import Hero2 from "./components/hero2/page"
 
 
 
@@ -25,10 +24,8 @@ export default function Home() {
         What's next on our coding adventure?
       </p>
       <Footer></Footer>
-      <Hero2/>
-      console.log("hi every one")
     </div>
   );
 }
 
-// console.log("hi every one")
+
